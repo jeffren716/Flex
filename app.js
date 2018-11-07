f20687cd71b9d6a8451ed4b12fdebbd7ca09e56c
jeffren716 <jeffyiren@gmail.com>
2018-11-06 23:10:54 -0800

    setup mongoose

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Test!"));
 
